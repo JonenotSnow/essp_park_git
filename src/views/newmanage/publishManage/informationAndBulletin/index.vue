8e637dff979f0ad5528b09ab699b79d1cd664037
15060766758 <1158694693@qq.com>
2019-01-05 16:56:29 +0800

    'tijiao'

---
+++ src/views/newmanage/publishManage/informationAndBulletin/index.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+     <!-- 数据管理 -->
+ {{msg}}
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+         msg:"数据管理"
+     }
+   },
+   created () {
+     
+   },
+   computed: {
+
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style lang='less' scoped >
+
+ 
+</style>
