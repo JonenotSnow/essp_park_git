039fc6b226785a7be797ed518ce52e3fe8584de4
brant9527 <398707650@qq.com>
2019-01-10 10:21:49 +0800

    添加工具判断

--- src/util/utils.js
+++ src/util/utils.js
@@ -514,7 +514,8 @@ const utils = {
     isBdPark: function () {
         //暂时依靠两种方案判断，（parkname 有保定 ，或者登陆带了 bdFlag 任意一个都算保定园区）
         var isParkNameHasBd = false;
-        let bdFlag = sessionStorage.getItem("bdParkId").indexOf('bd')>-1
+        let bdParkId = sessionStorage.getItem("bdParkId")
+        let bdFlag =  bdParkId && bdParkId.indexOf('bd')>-1
         let parkName = sessionStorage.getItem("parkName")
         if (parkName) {         
             var falg = parkName.indexOf("保定") > -1;
