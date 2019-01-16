1126ea82968bc6d0cdf8357a3838585b4704f2ce
Administrator <592732096@qq.com>
2019-01-16 21:45:57 +0800

    上传专家团队发布已经成果管理统一复用

--- src/fetch/api/achievement/achievement.js
+++ src/fetch/api/achievement/achievement.js
@@ -2,4 +2,7 @@ export const achievement = {
     addActivity: "/achiev/saveAchiev",  // 成果管理发布
     getAllAchiev:'/achiev/getAllAchiev',  // 全部成果列表
     delAchievByKey: '/achiev/delAchievByKey ',  // 删除成果列表
+    getExpertInfo: '/expert/getExpertInfo',        //获取全部专家列表
+    delExpert: '/expert/delExpert',                 // 删除专家
+    getExpertById:'/expert/getExpertById',          // 获取专家详情
 }
