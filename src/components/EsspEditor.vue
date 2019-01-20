6f90f47309dcb33cfd1527f4740bca438462bf01
brant9527 <398707650@qq.com>
2019-01-20 17:55:57 +0800

    修改编辑器配置

--- src/components/EsspEditor.vue
+++ src/components/EsspEditor.vue
@@ -1,76 +1,102 @@
 <!--<script src="../../../video_admin/config/index.js"></script>-->
 <template>
-    <quill-editor ref="myTextEditor" style="height: 400px;"  @keyup.tabprevent="onEditorTab" v-model="editorCont" @blur="onEditorBlur($event)" @change="onEditorChange($event)" :options="editorOption">
-        <div id="toolbar" slot="toolbar">
-         </div>
-    </quill-editor>
+  <quill-editor
+    ref="myTextEditor"
+    style="height: 400px;"
+    @keyup.tabprevent="onEditorTab"
+    v-model="editorCont"
+    @blur="onEditorBlur($event)"
+    @change="onEditorChange($event)"
+    :options="editorOption"
+  >
+    <div id="toolbar" slot="toolbar"></div>
+  </quill-editor>
 </template>
 
 <script>
+import { quillEditor } from "vue-quill-editor";
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
 
-    import {quillEditor} from 'vue-quill-editor'
-    import 'quill/dist/quill.core.css'
-    import 'quill/dist/quill.snow.css'
-    import 'quill/dist/quill.bubble.css'
-
-    export default {
-        name: 'editor',
-        components: {
-            quillEditor
-        },
-        props: ["editorCont"],
-        created(){
-            console.log(this.editorCont);
-        },
-        data() {
-            return {
-            	editorCon: this.editorCon || '',
-                editorOption: {
-                	editorCon: '',
-                    readOnly: 'true',
-                    placeholder: `请输入内容`
-                },
-            }
-        },
-        methods: {
-            // tab键
-            onEditorTab(event){
-                console.log(event);
-            },
-            // 失去焦点
-            onEditorBlur(editor){
-//                console.log("失去焦点",this.editorCon);
-//                var htmlStr = this.editorCon.replace(/\s/g, "&nbsp;&nbsp");
-                var htmlStr = this.editorCon;
-                this.$emit("onEditorChange", htmlStr);
-            },
-            // change事件
-            onEditorChange({editor,html,text}){
-                this.editorCon = html;
-                this.$emit("onEditorChange", this.editorCon);
-            }
-        },
-        computed:{
-            editor() {
-                return this.$refs.myTextEditor.quill
-            }
+export default {
+  name: "editor",
+  components: {
+    quillEditor
+  },
+  props: ["editorCont"],
+  created() {
+    console.log(this.editorCont);
+  },
+  data() {
+    return {
+      editorCon: this.editorCon || "",
+      editorOption: {
+        editorCon: "",
+        readOnly: "true",
+        placeholder: `请输入内容`,
+        modules: {
+          toolbar: {
+            container: [
+              ["bold", "italic", "underline", "strike"],
+              ["blockquote", "code-block"],
+              [{ header: 1 }, { header: 2 }],
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ script: "sub" }, { script: "super" }],
+              [{ indent: "-1" }, { indent: "+1" }],
+              [{ direction: "rtl" }],
+              [{ size: ["small", false, "large", "huge"] }],
+              [{ header: [1, 2, 3, 4, 5, 6, false] }],
+              [{ font: [] }],
+              [{ color: [] }, { background: [] }],
+              [{ align: [] }],
+              ["clean"],
+              ["link", "image"]
+            ]
+          }
         }
+      }
+    };
+  },
+  methods: {
+    // tab键
+    onEditorTab(event) {
+      console.log(event);
+    },
+    // 失去焦点
+    onEditorBlur(editor) {
+      //                console.log("失去焦点",this.editorCon);
+      //                var htmlStr = this.editorCon.replace(/\s/g, "&nbsp;&nbsp");
+      var htmlStr = this.editorCon;
+      this.$emit("onEditorChange", htmlStr);
+    },
+    // change事件
+    onEditorChange({ editor, html, text }) {
+      this.editorCon = html;
+      this.$emit("onEditorChange", this.editorCon);
     }
+  },
+  computed: {
+    editor() {
+      return this.$refs.myTextEditor.quill;
+    }
+  }
+};
 </script>
 <style lang="less">
-    .ql-container {
-        background: #fff;
-        height: -ms-calc(100% - 66px)!important;
-        height: -moz-calc(100% - 66px)!important;;
-        height: -webkit-calc(100% - 66px)!important;;
-        height: calc(100% - 66px)!important;;
-    }
-    .ql-container.ql-snow {
-        border: none!important;
-    }
-    .ql-container.ql-snow .ql-editor {
-        border: 1px solid #ccc!important;;
-    }
+.ql-container {
+  background: #fff;
+  height: -ms-calc(100% - 66px) !important;
+  height: -moz-calc(100% - 66px) !important;
+  height: -webkit-calc(100% - 66px) !important;
+  height: calc(100% - 66px) !important;
+}
+.ql-container.ql-snow {
+  border: none !important;
+}
+.ql-container.ql-snow .ql-editor {
+  border: 1px solid #ccc !important;
+}
 </style>
 
 
--- src/views/parkHall/needHall/publishNeed/index.vue
+++ src/views/parkHall/needHall/publishNeed/index.vue
@@ -83,7 +83,29 @@ export default {
       fileList: [],
       editorOption: {
         placeholder: "请输入模板内容",
-        theme: "snow"
+        theme: "snow",
+        modules:{
+                toolbar:
+                  {
+                    container:[
+                      ['bold', 'italic', 'underline', 'strike'],
+                      ['blockquote', 'code-block'],
+                      [{'header': 1}, {'header': 2}],
+                      [{'list': 'ordered'}, {'list': 'bullet'}],
+                      [{'script': 'sub'}, {'script': 'super'}],
+                      [{'indent': '-1'}, {'indent': '+1'}],
+                      [{'direction': 'rtl'}],
+                      [{'size': ['small', false, 'large', 'huge']}],
+                      [{'header': [1, 2, 3, 4, 5, 6, false]}],
+                      [{'font': []}],
+                      [{'color': []}, {'background': []}],
+                      [{'align': []}],
+                      ['clean'],
+                      ['link', 'image']
+                    ]
+                  }
+                
+            }
       },
       rules: {
         title: [
