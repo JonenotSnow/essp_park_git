4c34fe30dc2d49df53210e07728375c57d3db28a
jzb1205 <1158694693@qq.com>
2019-02-19 16:57:33 +0800

    '3'

--- src/views/newparkHall/home/noticeBar.vue
+++ src/views/newparkHall/home/noticeBar.vue
@@ -144,27 +144,9 @@
                     });
                 }
             },
-            // 点击任务池标题获取该审核权限
-            cancelAudit(id) {
-                this.$post(this.$apiUrl.manage.auditCancer, {
-                    entityId: id,
-                    type: "01",
-                    status: "01"
-                }).then(
-                    response => {
-                        if (response.resultCode == "CLT000000000") {
-                            this.$router.push({
-                                path: "/parkHall/manage/manageAuditing",
-                                query: {id: id}
-                            });
-                        } else {
-                            this.$message.error(response.resultMsg);
-                        }
-                    },
-                    response => {
-                        this.$message.error(response.resultMsg);
-                    }
-                );
+            //校验审核状态
+            cancelAudit(rows){
+                this.$router.push({path:'/parkHall/manage/manageParkAuditDetail',query:{entityId:rows.id,cstId:rows.cstId}});
             },
             goNoticeList() {
                 if (this.isBdPark) {
