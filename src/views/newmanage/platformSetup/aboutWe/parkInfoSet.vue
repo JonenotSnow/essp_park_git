90ac5dda688623075202de3f7e24c64f1b288973
jzb1205 <1158694693@qq.com>
2019-02-26 15:05:37 +0800

    'bug'

--- src/views/newmanage/platformSetup/aboutWe/parkInfoSet.vue
+++ src/views/newmanage/platformSetup/aboutWe/parkInfoSet.vue
@@ -144,16 +144,24 @@ export default {
                 });
                 return;
             }
-            // if (this.writeInfo.phoneNumber) {
-            //     let mb= /^([0-9]{3,4}-)?[0-9]{7,8}$/;
-            //     if (!mb.test(this.writeInfo.phoneNumber) ){
-            //         this.$message({
-            //             message: '固定电话格式不对',
-            //             type: 'warning'
-            //         });
-            //         return;
-            //     }
-            // }
+            if (this.writeInfo.phoneNumber) {
+                let mb= /^([0-9]{3,4}-)?[0-9]{7,8}$/; //固定电话
+                let tb= /^[1][3,4,5,7,8][0-9]{9}$/; //手机号码
+                if (this.writeInfo.phoneNumber.includes('-') && !mb.test(this.writeInfo.phoneNumber)){
+                    this.$message({
+                        message: '固定电话格式不对',
+                        type: 'warning'
+                    });
+                    return;
+                }
+                if (!tb.test(this.writeInfo.phoneNumber)){
+                    this.$message({
+                        message: '该手机号码不存在',
+                        type: 'warning'
+                    });
+                    return;
+                }
+            }
             if (!this.writeInfo.email) {
                 this.$message({
                     message: '联系邮箱不能为空',
